4c8ecc116022e01a7a4985b67972b84e8f76bc28
MisterX4507 <koryshev1997@gmail.com>
2017-04-10 00:29:54 +0700

    Добавление ООП

--- practicMP4/practicMP4/codetree.cpp
+++ practicMP4/practicMP4/codetree.cpp
@@ -3,44 +3,51 @@
 #include <time.h>
 #include <Windows.h>
 using namespace std;
-int i, maxlevel;
-struct Node{
+int i;
+class TREE {
+private:
+	struct Node{
 	int data;
 	Node *children[5];
-};
-
-Node * rec_addNode(int * data, Node * curNode) {
+	};
+public:
+	int maxlevel;	
+	Node * root = NULL;
+	Node * rec_addNode(int * data, Node * curNode) {
 	if (!curNode) {
 		curNode = new Node;
 		curNode->data = *data;
 		for (i = 0; i <= 4; i++) {
 			curNode->children[i] = NULL;
-		} cout << curNode->data << "\n";
+		}
 		return curNode;
 	}
-	int accidentn = rand() % 5; cout << accidentn << " ";
-	curNode->children[accidentn] = rec_addNode(data, curNode->children[accidentn]); cout << curNode->data << "€€\n";
+	int accidentn = rand() % 5;
+	curNode->children[accidentn] = rec_addNode(data, curNode->children[accidentn]);
 	return curNode;
-}
-void printing(Node * root, int level){
+	}
+	void printing(Node * root, int level){
 	if (root){
-		cout << "level=" << level << " data=" << root->data << "\n";
+		cout << "”ровень = " << level << " „исло = " << root->data << "\n";
 		if (level > maxlevel) { maxlevel = level; }
 		int k;
-		for (k = 0; k <= 4; ++k){
+		for (k = 0; k <= 4; k++){
 			printing(root->children[k], level + 1);
 		}
 	}
-}
+	}
+};
 int main() {
 	setlocale(LC_ALL, "Russian");
 	srand((unsigned int)time(0));
-	Node * root = NULL;
+	TREE a;
 	for (int j = 1; j <= 12; j++) {
 		int put; put = rand() % 50;
-		cout << "„исло, которое пытаютс€ запихать " << put << " "; root = rec_addNode(&put, root);
-	} maxlevel = -1;
-	printing(root, 0);
-	cout << "√лубина дерева: " << maxlevel;
+		a.root = a.rec_addNode(&put, a.root);
+	}
+	a.maxlevel = -1;
+	a.printing(a.root, 0);
+	cout << "√лубина дерева: " << a.maxlevel << "\n";
+	system("pause");
 	return 0;
 }
